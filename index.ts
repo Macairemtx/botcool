6346988c4338d60b48f2bc0c3e764722824880fb
Macairemtx <95448204+Macairemtx@users.noreply.github.com>
2025-10-07 04:11:59 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,23 +1,28 @@
 import { APP_URL, PORT, TELEGRAM_TOKEN } from "./constants";
-import express, { Application } from "express";
-import { Telegraf } from "telegraf";
-const bot = new Telegraf(TELEGRAM_TOKEN);
+import express, { Application, Request, Response } from "express";
+import { Telegraf, Context } from "telegraf";
+
+const bot = new Telegraf<Context>(TELEGRAM_TOKEN);
 const app: Application = express();
+
 app.use(express.static("static"));
 app.use(express.json());
-app.get("/", (_, res) => {
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
+
 app.listen(PORT, () => {
-  console.log(`Server is Fire at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
-bot.command("start", (ctx) => {
-  return ctx.reply(`Play Cool Frog!`, {
+
+bot.command("start", (ctx: Context) => {
+  return ctx.reply("Play Cool Frog!", {
     reply_markup: {
       inline_keyboard: [
         [
           {
-            text: `Play Game`,
+            text: "Play Game",
             web_app: { url: `${APP_URL}/` },
           },
         ],
@@ -25,5 +30,7 @@ bot.command("start", (ctx) => {
     },
   });
 });
+
 bot.launch();
+
 export default app;
